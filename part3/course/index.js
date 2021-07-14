0f9da3bfc351513229c867acc367d3ab6d8b45b7
allthetok <al.tokjuman@gmail.com>
2021-07-13 20:55:37 -0400

    backend hosting

--- part3/course/index.js
+++ part3/course/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
 
 app.use(express.json());
 
@@ -82,7 +86,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
